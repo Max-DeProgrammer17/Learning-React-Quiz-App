4934f30389ba086529f9267feaaaea878958c177
Efoe Max <efoemaxi17@gmail.com>
2025-04-28 10:03:58 +0100

    first

---
+++ JSX/src/App.jsx
@@ -0,0 +1,15 @@
+import Head from './atom/head.atom'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='main-container'>
+<Head />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ JSX/src/atom/head.atom.jsx
@@ -0,0 +1,16 @@
+import './head.atom.css'
+function Head(){
+    return(
+        <>
+        <div className='head-container'>
+            <h1>Quiz App</h1>
+        <h3>Question 1</h3>
+        <p>What is the capital of France? </p>
+        </div>
+         
+        </>
+       
+    )
+}
+
+export default Head;
